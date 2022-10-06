565b4979f5f2aa53add1a6968d8f8fcef4ca9086
Deigon Prates <deigon_prates@hotmail.com>
2022-10-06 20:53:21 -0300

    crud de tipo

--- app/controllers/TipoControler.php
+++ app/controllers/TipoControler.php
@@ -10,20 +10,101 @@ class TipoControler {
     }
 
     public function list(){
-        $result = $this->DB->query("SELECT * FROM tipos ORDER BY nome ASC ")->fetchAll();
+        $result = $this->DB->query("SELECT * FROM tipos ORDER BY id DESC ")->fetchAll(PDO::FETCH_OBJ);
         echo json_encode($result);
     }
 
-    public function create(){}
+    public function create(){
+        $erros = $this->validAll();
+        
+        if(count($erros) > 0){
+            echo json_encode($erros);
+            http_response_code(406);
+        }else{
+            $this->DB->beginTransaction();
+            $stmt = $this->DB->prepare("INSERT INTO tipos (nome,imposto) VALUES(:nome,:imposto)");
+            $stmt->bindParam(':nome',    $_POST['nome'], PDO::PARAM_STR);
+            $stmt->bindParam(':imposto', $_POST['imposto'], PDO::PARAM_STR);
+        
+            if ($stmt->execute()) {
+                $this->DB->commit();
+                $this->read($this->DB->lastInsertId());
+                http_response_code(201);
+            }else{
+                $this->DB->rollBack();
+                echo json_encode($this->db->errorInfo());
+                http_response_code(500);
+            }
+        }
+              
+    }
 
     public function read($id){
-        $result = $this->DB->query("SELECT * FROM tipos WHERE id = $id")->fetch();
+        $result = $this->DB->query("SELECT * FROM tipos WHERE id = $id")->fetch(PDO::FETCH_OBJ);
         echo json_encode($result);
     }
 
-    public function update($id){}
+    public function update($id){
+        $erros = $this->validAll();
+        if(!is_numeric($id) || empty($id)){
 
-    public function delete($id){}
+            echo json_encode(['Tipo inválido']);
+            http_response_code(401);
+        }
+        if(count($erros) > 0){
+            echo json_encode($erros);
+            http_response_code(406);
+        }else{
+            $this->DB->beginTransaction();
+            $stmt = $this->DB->prepare("UPDATE tipos set nome=:nome, imposto=:imposto where id=:id");
+            $stmt->bindParam(':id',      $id, PDO::PARAM_INT);
+            $stmt->bindParam(':nome',    $_REQUEST['nome'], PDO::PARAM_STR);
+            $stmt->bindParam(':imposto', $_REQUEST['imposto'], PDO::PARAM_STR);
+        
+            if ($stmt->execute()) {
+                $this->DB->commit();
+                $this->read($id);
+                http_response_code(200);
+            }else{
+                $this->DB->rollBack();
+                echo json_encode($this->db->errorInfo());
+                http_response_code(500);
+            }
+        }
+    }
+
+    public function delete($id){
+        try {
+            $this->DB->beginTransaction();
+            $stmt = $this->DB->prepare("DELETE FROM tipos WHERE id = :id");
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);        
+            if ($stmt->execute()) {
+                    $this->DB->commit();
+                    echo json_encode(['Removido com sucesso']);
+                    http_response_code(200);
+            }else{
+                    $this->DB->rollBack();
+                    echo json_encode($this->db->errorInfo());
+                    http_response_code(500);
+            }
+        }catch (\Exception $e) {
+            echo json_encode($e);
+            http_response_code(500);
+        }
+    }
+
+    private function validAll(){
+        $erros = [];
+        if(!isset($_REQUEST['nome']) || empty($_REQUEST['nome'])){
+            $erros[] = ['O nome é obrigatório'];
+        }
+        if(!isset($_REQUEST['imposto']) || empty($_REQUEST['imposto'])){
+            $erros[] = ['O imposto é obrigatório'];
+
+        }
+        
+        return $erros;
+    }
 }
 
 
