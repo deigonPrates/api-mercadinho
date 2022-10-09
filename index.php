f758650b24af06988c2516e9d565dd14169d4175
Deigon Prates <deigon_prates@hotmail.com>
2022-10-09 17:01:04 -0300

    refatorando o codigo

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <?php
+
 include_once('cors.php');
-include_once('./app/router/ProdutoRotas.php');
-include_once('./app/router/TipoRotas.php');
-include_once('./app/router/VendaRotas.php');
-include_once('./app/router/HistoricoComprasRotas.php');
+require 'vendor/autoload.php';
 
 $url = explode('/',$_SERVER['REQUEST_URI']);
 
 switch ($url[1]) {
 	case 'produto':
-		ProdutoRotas::boot();
+        \app\router\ProdutoRotas::boot();
 	break;
 	case 'tipo':
-		TipoRotas::boot();
+        \app\router\TipoRotas::boot();
 	break;
 	case 'venda':
-		VendaRotas::boot();
+        \app\router\VendaRotas::boot();
 	break;
 	case 'historico':
-		HistoricoComprasRotas::boot();
+        \app\router\HistoricoComprasRotas::boot();
 	break;
 	default:
 		http_response_code(405);
